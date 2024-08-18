1812f1dfaf0be559d58a7a67df24d0c5885635d6
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-08-18 19:24:59 +0530

    jwt tokens added

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,38 +1,50 @@
 import { Router } from "express";
 import generateDigitalNumber from "../utils/digitalNumber.js";
 import { prisma } from '../db/db.js'
+import jwt from 'jsonwebtoken'
+
 const auth = Router()
 
 auth.post('/register', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+
+    try{
+        const duplicate = await prisma.user.findFirst({
+            where:{
+                name: username
+            }
+        })
     
-    const duplicate = await prisma.user.findFirst({
-        where:{
-            name: username
+        if (duplicate) {
+            return res.status(400).json({
+                msg: 'User already exists',
+            });
         }
-    })
+    
+        const digitalNumber = generateDigitalNumber();
+    
+        await prisma.user.create({
+            data:{
+                name: username,
+                digitalNumber,
+                password
+            }
+        })
 
-    if (duplicate) {
-        return res.status(400).json({
-            msg: 'User already exists',
-        });
-    }
-
-    const digitalNumber = generateDigitalNumber();
-
-    await prisma.user.create({
-        data:{
-            name: username,
+        const token = jwt.decode(digitalNumber)
+    
+        return res.status(200).json({
+            msg: 'User created successfully',
             digitalNumber,
-            password
-        }
-    })
-
-    return res.status(200).json({
-        msg: 'User created successfully',
-        digitalNumber,
-    });
+            token
+        });
+    }catch(e){
+        return res.status(500).json({
+            msg: "Server error"
+        })
+    }
+    
 
 });
 
@@ -40,22 +52,31 @@ auth.post('/login', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = await prisma.user.findFirst({
-        where:{
-            name : username,
-            password
+    try{
+        const user = await prisma.user.findFirst({
+            where:{
+                name : username,
+                password
+            }
+        })
+    
+        if(!user){
+            return res.status(404).json({
+                msg: "User not found"
+            })
         }
-    })
+        const token = jwt.decode(digitalNumber)
 
-    if(!user){
-        return res.status(404).json({
-            msg: "User not found"
+        return res.status(200).json({
+            msg: 'Login success',
+            token
+        });
+    }
+    catch(e){
+        return res.status(500).json({
+            msg: "Server Error"
         })
     }
-
-    return res.status(200).json({
-        msg: 'Login success',
-    });
 });
 
 export default auth
